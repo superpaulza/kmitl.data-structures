237aa55b4fb0a64560e35d3b349ab193bed11381
unknown <166_35049@st.bcc1852.com>
2020-09-07 15:51:28 +0700

    Final

--- src/MyArray.java
+++ src/MyArray.java
@@ -17,30 +17,43 @@ public class MyArray {
 	}
 	
 	int find(int data) {
-		
+		for (int i = 0; i < S; i++) {
+			if (a[i] == data) return i;
+		}
+		return -1;
 	}
 	
 	void delete(int index) {
-		
+		a[index] = a[--n];
 	}
 	
 	void deleteA(int index) {
-		
+		for (int i = index; i < n-1; i++) {
+			a[i] = a[i+1];
+		}
+		n--;
 	}
 	
 	boolean IsFull(int data) {
-		
+		return n==S;
 	}
 	
 	boolean IsEmpty(int data) {
-		
+		return n==0;
 	}
 	
 	String toString(int data) {
-		
+		String st = "[";
+		if (n>0) st+=a[0];
+		for (int i = 1; i < n; i++) {
+			st+=","+a[i];
+		}
+		st+="]";
+		return st;
 	}
 	
 	public static void main(String[] args) {
+	
 	}
 	
 	
